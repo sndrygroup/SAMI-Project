b0c5f0caf7e88395579404c000c5126c7538f901
sndrygroup <maramalarifi@outlook.com>
2026-03-02 02:58:05 +0300

    Update app\layout.tsx

--- app\layout.tsx
+++ app\layout.tsx
@@ -1,1 +1,21 @@
+import "./globals.css";
+import Providers from "./components/providers";
 
+export const metadata = {
+  title: "SAMI - مساعدك التسويقي الذكي",
+  description: "SAMI هو مساعدك التسويقي الذكي، مبني ليجمع بين التقنية، الإبداع، والتحليل.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body className="bg-[#0A090C] text-[#D1D1D6]">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
